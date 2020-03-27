780507d09476e94d17f834f506e62b178c8ee284
kirakrishnan <aravindakrishnan92@gmail.com>
2020-03-26 20:32:08 -0400

    adding conditoin for search slug

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,14 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    if category_slug:
+    if category_slug == "search":
+        print("##### entering the Search condition #######")
+        query = request.GET['search']
+        print(query)
+        print("update the query to search in sql lit based on the query value and store it in products variable")
+        category = get_object_or_404(Category, slug='clutches')
+        products = products.filter(category=category)
+    elif category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request,
